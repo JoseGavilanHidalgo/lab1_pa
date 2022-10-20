19b03337267571b0e4b16474715f242b9d6a0456
Jose Javier Gavilan Hidalgo <jose.gavilan@alumnos.ucentral.cl>
2022-10-20 15:27:38 -0300

    ...

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,135 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Mensajes</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-searchbar color=""></ion-searchbar>
+      <ion-card button="true" color="success">
+        <ion-card-header>
+          <img src="./personal_medico.png" class="img1"/>
+          <ion-card-title class="nombre">Dra. María
+          </ion-card-title>
+            <ion-card-subtitle class="rol">Personal Medico
+            </ion-card-subtitle>
+            <br>
+        </ion-card-header>
+        <ion-card-content style="display:block">
+            <p class="mensaje"> <time style="float:left;padding-right: 10px; ">11:25am</time>¿Como te has sentido hoy?  <ion-badge style="float:right" class="badges">3</ion-badge></p>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card button="true" color="warning">
+        <ion-card-header>
+          <img src="./cuidador.png" class="img1"/>
+          <ion-card-title class="nombre">Laura</ion-card-title>
+            <ion-card-subtitle class="rol">Cuidador</ion-card-subtitle><br>
+        </ion-card-header>
+        <ion-card-content style="display:block">
+            <p class="mensaje"> <time style="float:left;padding-right: 10px; ">11:25am</time>¿Como te has sentido hoy?  <ion-badge style="float:right" class="badges">3</ion-badge></p>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card button="true" color="success">
+        <ion-card-header>
+          <img src="./personal_medico.png" class="img1"/>
+          <ion-card-title class="nombre">Dr. Juan</ion-card-title>
+            <ion-card-subtitle class="rol">Personal Medico</ion-card-subtitle>
+            <br>
+        </ion-card-header>
+        <ion-card-content style="display:block">
+            <p class="mensaje"> <time style="float:left;padding-right: 10px; ">11:25am</time>¿Como te has sentido hoy?  <ion-badge style="float:right" class="badges">3</ion-badge></p>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card button="true" color="warning">
+        <ion-card-header>
+          <img src="./cuidador.png" class="img1"/>
+          <ion-card-title class="nombre">Roberto</ion-card-title>
+            <ion-card-subtitle class="rol">Cuidador</ion-card-subtitle><br>
+        </ion-card-header>
+        <ion-card-content style="display:block">
+            <p class="mensaje"> <time style="float:left;padding-right: 10px; ">11:25am</time>¿Como te has sentido hoy?  <ion-badge style="float:right" class="badges">3</ion-badge></p>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-card button="true" color="success">
+        <ion-card-header>
+          <img src="./personal_medico.png" class="img1" />
+          <ion-card-title class="nombre">Dr. Juan</ion-card-title>
+            <ion-card-subtitle class="rol">Personal Medico</ion-card-subtitle><br>
+        </ion-card-header>
+        <ion-card-content style="display:block">
+            <p class="mensaje"> <time style="float:left;padding-right: 10px; ">11:25am</time>¿Como te has sentido hoy?  <ion-badge style="float:right" class="badges">3</ion-badge></p>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {IonBadge, IonContent, IonHeader, IonPage, IonTitle, IonToolbar ,IonSearchbar} from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'HomePage', 
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonBadge,
+    IonSearchbar,
+    IonToolbar,
+  }
+});
+</script>
+
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+.img1 {
+  height: 80px;
+  width: 100px;
+  float:left;
+  clear:left;
+  margin-right: 20px;
+}
+
+.nombre{
+  color:black;
+}
+.rol {
+  color:black;
+}
+
+.mensaje {
+  padding-top:5px;
+}
+</style>
---
+++ src/views/TabsPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="home" />
+          <ion-label>Inicio</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="mail" />
+          <ion-label>Mensajes</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="calendar" />
+          <ion-label>Calendario</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="person"></ion-icon>
+          <ion-label>Perfil</ion-label>
+        </ion-tab-button>
+
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { ellipse, square, triangle ,home , calendar ,person , mail} from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'TabsPage',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  setup() {
+    return {
+      ellipse, 
+      square, 
+      triangle,
+      home ,
+      calendar,
+      person,
+      mail
+    }
+  }
+});
+</script>
